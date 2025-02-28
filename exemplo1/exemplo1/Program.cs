e0509387eb952add56762c0f2338f2dd416b3771
Roberty345 <robertyrodrigo2@gmail.com>
2025-02-28 15:57:20 +0000

    Add files via upload

---
+++ exemplo1/exemplo1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class exemplo1
+{
+    public static void Main(string[] args)
+    {
+        //declarar variaveis
+        int num1 = 20;
+        int num2 = 30;
+
+        int soma = num1 + num2;
+        int sub = num1 - num2;
+        int multi = num1 * num2;
+        int div = num1 / num2;
+        int resto = num1 % num2;
+
+        Console.WriteLine(soma);
+        Console.WriteLine(multi);
+        Console.WriteLine(div);
+        Console.WriteLine(sub);
+        Console.WriteLine(resto);
+
+
+
+
+    }
+
+}  
