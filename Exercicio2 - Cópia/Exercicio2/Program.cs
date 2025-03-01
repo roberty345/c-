46b9537fe3386854fd60b82ca7819e1e5f6423b9
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-01 14:10:50 +0000

    Add files via upload

---
+++ Exercicio2 - Cópia/Exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class exercicio2
+{
+    
+    public static void Main(string[] args)
+     {
+       double Raio = 10;
+       double diametro = 2 * Raio;
+       Console.WriteLine("O diametro e:" + diametro);
+
+
+
+     }
+}
