47fc4b9352972a9050f3f64a12a69ff1058261cc
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-01 14:42:54 +0000

    Add files via upload

---
+++ ifexemplo/ifexemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class ifexemplo
+{
+    public static void Main(string[] args)
+    {
+        //exercicio1
+        for (int i = 0; i > -1; i++)
+        {
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
