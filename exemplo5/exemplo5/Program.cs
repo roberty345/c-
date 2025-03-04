fdc94baa782ade5eaa898d5b1e8107945008db21
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 22:41:03 +0000

    Add files via upload

---
+++ exemplo5/exemplo5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class exemplo5
+{
+    public static void Main(string[] args)
+    {
+        int X = 5;
+
+        switch (X)
+        {
+            case 1:
+                Console.WriteLine("Domingo");
+                break;
+
+            case 2:
+                Console.WriteLine("Segunda-feira");
+                break;
+
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+
+            case 7:
+                Console.WriteLine("Sabado");
+                break;
+
+            default:
+                Console.WriteLine("Numero invalido, introduza um numero de 1 a 7");
+              break;
+        }
+    }
+}
