13dddde7cb69a89ffda0cee1e32438bc109b761b
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 23:28:53 +0000

    Add files via upload

---
+++ exercicio_10/exercicio_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class exercicio10
+{
+    public static void Main(string[] args)
+    {
+        //Esta variavel e um contador que vai ate 10
+        int contador = 0;
+
+        //Logica do exercicio 10
+        do
+        {
+            Console.WriteLine(contador);
+            //Isto aumenta o numero do contador em 1
+            contador++;
+        }
+        while (contador <= 10);
+
+
+
+
+    }
+}
