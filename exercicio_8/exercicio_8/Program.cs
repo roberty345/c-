629a17a19ed6ad8be852439106c07f41e71da612
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 23:09:03 +0000

    Add files via upload

---
+++ exercicio_8/exercicio_8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class exercicio_8
+{
+    public static void Main(string[] args)
+    {
+        for (int n = 1; n <= 10; n++)
+        {
+            int x = n * 3;
+            Console.WriteLine("Multiplo: " + x);
+        }
+
+    }
+}
