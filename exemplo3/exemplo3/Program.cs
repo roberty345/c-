5d9775214c24db068c76e677ad27f67f1cc6de4d
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 22:27:51 +0000

    Add files via upload

---
+++ exemplo3/exemplo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class exercicio3
+{
+    //exercicio 3
+    public static void Main(string[] args)
+    {
+        int idade = 20;
+        bool eCasado = true;
+
+        if (idade < 18)
+        {
+            Console.WriteLine("É menor de idade");
+        }
+        else if (idade >= 18 && idade < 65)
+        {
+            if (eCasado)
+            {
+                Console.WriteLine("É casado");
+            }
+            else if (!eCasado)
+            {
+                Console.WriteLine("É solteiro");
+            }
+            Console.WriteLine("É adulto");
+        }
+
+        else
+        {
+            Console.WriteLine("È adulto e solteiro");
+        }
+
+
+    }
+}
