acffdd3b519e45a2d12083dc2839b110f596dcec
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 22:59:34 +0000

    Add files via upload

---
+++ exemplo7/exemplo7/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    public class exemplo7
+    {
+        public static void Main(string[] args)
+        {
+            int l = 0;
+            switch (l)
+            {
+                case >= 18 and <= 20:
+                    Console.WriteLine("Exelente");
+                    break;
+                case >= 14 and <= 17:
+                    Console.WriteLine("Bom");
+                    break;
+                case >= 10 and <= 13:
+                    Console.WriteLine("Suficiente");
+                    break;
+                case >= 0 and <= 10:
+                    Console.WriteLine("Insuficiente");
+                    break;
+                default:
+                    Console.WriteLine("Nota invalida");
+                    break;
+
+            }
+
+        }
+    }
+}
+
