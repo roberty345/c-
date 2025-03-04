aed0485911913574f87cdedb3ed4158bfdce8c3a
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 23:19:26 +0000

    Add files via upload

---
+++ exercicio_9/exercicio_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class exercicio9
+{
+    public static void Main(string[] args)
+    {
+        int soma = 1;
+        int cont = 1;
+
+        while (soma <= 100)
+        {
+            cont++;
+            soma += cont;
+            Console.WriteLine(cont + " = " + soma);
+        }
+
+    }
+}
