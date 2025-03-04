de6a8559e775a48dec8592f34c5e35428f2fc25b
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-04 22:17:14 +0000

    Add files via upload

---
+++ exemplo2D22/exemplo2D22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class exemplo2D22
+{
+    //exemplo2D22
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+        for (int i = 1; i < 50; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma = soma + i;
+            }
+        }
+        Console.WriteLine("total é " + soma);
+    }
+}
