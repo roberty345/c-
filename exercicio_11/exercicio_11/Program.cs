fd1d21f08c9a2949ecc157f36e0552f50f37c321
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-05 13:06:06 +0000

    Add files via upload

---
+++ exercicio_11/exercicio_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class exercicio_11
+{
+    public static void Main(string[] args)
+    {
+        int contador = 0;
+
+        do
+        {
+            Console.WriteLine(contador);
+
+            contador++;
+        }
+        while (contador <= 5);
+        //Depois de chegar a 5 a leitura do console le depois do while
+        Console.WriteLine("Contagem concluida");
+
+    }
+}
