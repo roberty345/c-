aee11f03dc276b5b8c97a3dc879ea124404e4e72
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-05 13:23:27 +0000

    Add files via upload

---
+++ exemplo_Continue/exemplo_Continue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class exemplo_Continue
+{
+    public static void Main(string[] args)
+    {
+        for (int contador = 0; contador <= 10; contador++)
+        {
+            if (contador == 5)
+            {
+                continue;
+            }
+            Console.WriteLine(contador);
+        }
+
+
+    }
+}
