b1b2a7a726d68bffd9ac68015e405d6a09f3de7d
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-05 13:17:32 +0000

    Add files via upload

---
+++ exemplo_Break/exemplo_Break/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class exemplo_Break
+{
+
+    public static void Main(string[] args)
+    {
+        //Loop Aninhado
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            if (contador == 5)
+            {
+                break;
+            }
+            Console.WriteLine(contador);
+        }
+
+    }
+}
