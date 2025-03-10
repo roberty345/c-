41623985d42a46ad85f0251f797772068e7bfacc
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-10 17:28:20 +0000

    Add files via upload

---
+++ exercicio_14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class exercicio_14 
+{
+
+    public static void Main(string[] args)
+    {
+
+        int resultado = MaiorMenor(30, 10);
+
+        Console.WriteLine("Dos numeros indicados, " + resultado + " é o maior !");
+
+
+
+    }
+    public static int MaiorMenor(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
