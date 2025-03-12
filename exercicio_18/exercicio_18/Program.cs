f8dc296e3ef30464ea2c49e227a73022927f1873
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-12 09:02:24 +0000

    Add files via upload

---
+++ exercicio_18/exercicio_18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class exercicio_18
+{
+    public static void Main(String[] args)
+    {
+        // arrays
+
+        String[] cidades = { "Lisboa", "Porto", "Coimbra", "Faro", "braga" };
+        int[] numerodepopulacao = { 545000, 237000, 143000, 118000, 193000 };
+
+        // coisas que aparecem na tela
+
+        Console.WriteLine("Cidades e as suas populações: " + "\n");
+
+        for (int contador = 0; contador < cidades.Length; contador++)
+        {
+            Console.WriteLine($"{cidades[contador]} - {numerodepopulacao[contador]} Habitantes");
+        }
+    }
+}
