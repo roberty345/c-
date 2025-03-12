f44aac103438b94f2b975257b01fba61c9ba3dc9
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-12 08:54:39 +0000

    Add files via upload

---
+++ exercicio_15/exercicio_15/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿using System;
+
+public class exercicio_15
+{
+    public static void Main(string[] args)
+    {
+        //dados
+
+        bool resultado = ParImpar (10);
+
+        //logca
+
+        if (resultado == true)
+        {
+            Console.WriteLine("O número é par");
+        }
+        else
+        {
+            Console.WriteLine("O número é impar");
+        }
+    }
+    public static bool ParImpar(int a) 
+    {
+        return (a % 2 == 0);
+    }
+}
