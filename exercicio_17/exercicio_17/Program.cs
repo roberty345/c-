65cf98e6ef3c116123bb09395acf170fddb413c3
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-12 08:59:44 +0000

    Add files via upload

---
+++ exercicio_17/exercicio_17/Program.cs
@@ -0,0 +1,22 @@
+﻿
+﻿using System;
+
+public class exercicio_17
+{
+    public static void Main(string[] args)
+    {
+        // arrays
+
+        string[] nomefilmes = { "Harry potter", "Zorro", "Simpsons" };
+        int[] anofilmes = { 1999, 2010, 2014 };
+
+        // coisas que aparecem na tela
+
+        Console.WriteLine("Lista de filmes: ");
+
+        for (int contador = 0; contador < anofilmes.Length; contador++)
+        {
+            Console.WriteLine(nomefilmes[contador] + " - " + anofilmes[contador]);
+        }
+    }
+}
