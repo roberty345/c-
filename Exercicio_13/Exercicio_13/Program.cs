84d639e324f480062a4a669bb31c482d51daf223
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-21 11:44:04 +0000

    Add files via upload

---
+++ Exercicio_13/Exercicio_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercicio_13
+{
+   public static void Main(String []args)
+    {
+        int numero = 1;
+
+        do
+        {
+            Console.WriteLine(numero);
+            numero++;
+        } while (numero <= 5);
+
+        Console.WriteLine("Contagem concluída!");
+    }
+}
