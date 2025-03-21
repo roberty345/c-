d398f42be13be1d3d2e4164fdfa88fabd6aca6be
Roberty345 <robertyrodrigo2@gmail.com>
2025-03-21 11:57:49 +0000

    Add files via upload

---
+++ Exercicio_12/Exercicio_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Exercico_12
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+    }
+}
