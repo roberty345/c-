18348905d176c73bf6d20b0fa38bf8cb3b9173ac
Roberty345 <robertyrodrigo2@gmail.com>
2025-04-02 11:30:44 +0100

    Add files via upload

---
+++ exercicio_19/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Exercicio_19
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, string> listaTelefonica = new Dictionary<string, string>();
+        string opcao;
+
+        do
+        {
+
+            Console.WriteLine("\n1. Adicionar contactos");
+            Console.WriteLine(" \n2. Pesquisar contacto");
+            Console.WriteLine(" \n3. Remover contactos");
+            Console.WriteLine(" \n4. Sair");
+            Console.WriteLine("Escolha uma opção:");
+
+            opcao = Console.ReadLine();
+            int opcaoInt = Convert.ToInt32(opcao);
+            switch (opcaoInt)
+            {
+                case 1:
+                    Console.WriteLine("Nome:");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine("Contacto:");
+                    string contacto = Console.ReadLine();
+                    int contactoInt = Convert.ToInt32(contacto);
+                    listaTelefonica.Add(nome, contactoInt.ToString());
+                    Console.WriteLine("Contacto adicionado!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Pesquisar contacto:");
+                    string pesquisacontacto = Console.ReadLine();
+
+                    if (listaTelefonica.ContainsKey(pesquisacontacto))
+                    {
+                        Console.WriteLine($"\nTelefone: {listaTelefonica[pesquisacontacto]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contacto não encontrado.");
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("Remover contacto:");
+                    pesquisacontacto = Console.ReadLine();
+
+                    if (listaTelefonica.ContainsKey(pesquisacontacto))
+                    {
+                        listaTelefonica.Remove(pesquisacontacto);
+                        Console.WriteLine("Contacto removido.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contacto não encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    Console.WriteLine("Saindo...");
+                    opcao = "4";
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    break;
+            }
+        }
+        while (opcao != "4");
+
+    }
+}
